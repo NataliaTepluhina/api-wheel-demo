04768bcebf9e7156af25e323fd4a6c1bcf930b31
NataliaTepluhina <tarya.se@gmail.com>
2022-10-04 21:21:00 +0200

    feat: added query deduplication

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="fetchData">Click me</button>
+    <p v-if="loading">Loading...</p>
+  </div>
+</template>
+
+<script>
+import { useFetch } from '../composables/useFetch'
+import axios from 'axios'
+export default {
+  name: 'App',
+  setup() {
+    const { loading, fetchData } = useFetch(() =>
+      axios
+        .get(`https://api.thecatapi.com/v1/images/search`)
+        .then(response => response.data[0].url)
+    )
+
+    return { loading, fetchData }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  padding-top: 20px;
+}
+</style>
